fb8414fed7608693cb1e932d5380415cb0ae5b7c
thiagoMoraes21 <thiago4x70@gmail.com>
2020-09-11 00:12:18 -0300

    isolating express config with consign package

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const express = require('express');
-const app = express();
+const customExpress = require('./config/customExpress');
 const PORT = process.env.PORT || 3000;
+
+const app = customExpress();
 
 app.listen(PORT, () => {
     console.log(`Server is running at port: ${PORT}`);
-});
-
-app.get('/atendimentos', (req, res) => {
-    res.send('Você está na rota de atendimentos.');
 });

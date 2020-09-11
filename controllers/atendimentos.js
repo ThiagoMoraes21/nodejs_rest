e79d2447ab7b22f5abb30c26edff83598824859b
thiagoMoraes21 <thiago4x70@gmail.com>
2020-09-11 00:28:31 -0300

    add body parser and a new endpoint for atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,9 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => {
         res.send('Você está na rota de atendimentos.');
     });
+
+    app.post('/atendimentos', (req, res) => {
+        console.log('PAYLOAD: ', req.body);
+        res.send('Você está na rota de atendimentos e está realizando um POST.');
+    });
 }

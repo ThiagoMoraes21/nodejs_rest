fbc4a077f844ddba529e4822610bd1f869db9b09
thiagoMoraes21 <thiago4x70@gmail.com>
2020-09-14 20:41:46 -0300

    add criation dates for atendimentos table

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,14 @@
+const moment = require('moment');
 const connection = require('../infrastructure/connection');
 
 class Atendimento {
     add(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+        const atendimentoDatado = { ...atendimento, dataCriacao, data };
         const sql = `INSERT INTO Atendimentos SET ?`;
 
-        connection.query(sql, atendimento, (err, results) => {
+        connection.query(sql, atendimentoDatado, (err, results) => {
             if(err)
                 console.log('Err: ', err);
 

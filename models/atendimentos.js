48ddb5e21d466b4824f0b03c484a2b1cbfb8dfbc
thiagoMoraes21 <thiago4x70@gmail.com>
2020-09-14 20:50:52 -0300

    add http status code on responses

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,17 +2,17 @@ const moment = require('moment');
 const connection = require('../infrastructure/connection');
 
 class Atendimento {
-    add(atendimento) {
+    add(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
         const atendimentoDatado = { ...atendimento, dataCriacao, data };
         const sql = `INSERT INTO Atendimentos SET ?`;
 
-        connection.query(sql, atendimentoDatado, (err, results) => {
-            if(err)
-                console.log('Err: ', err);
+        connection.query(sql, atendimentoDatado, (error, results) => {
+            if(error)
+                return res.status(400).json({ error });
 
-            console.log('Results: ', results);
+            res.status(201).json({ results });
         });
     }
 }

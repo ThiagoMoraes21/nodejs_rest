45809f7524e7b13dd44d0e0a12c4a70b5551f5b7
thiagoMoraes21 <thiago4x70@gmail.com>
2020-09-14 21:06:06 -0300

    add params validations

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,6 +5,28 @@ class Atendimento {
     add(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+        
+        const validDate = moment(data).isSameOrAfter(dataCriacao);
+        const validClientName = atendimento.cliente.length >= 5;
+
+        const validations = [
+            {
+                name: 'data',
+                valid: validDate,
+                message: 'Data deve ser maior ou igual a data atual.'
+            },
+            {
+                name: 'cliente',
+                valid: validClientName,
+                message: 'Nome do cliente deve ter no mínimo 5 caracteres.'
+            }
+        ];
+
+        const errors = validations.filter(field => !field.valid);
+        
+        if(errors.length > 0) 
+            return res.status(400).json({ errors });
+        
         const atendimentoDatado = { ...atendimento, dataCriacao, data };
         const sql = `INSERT INTO Atendimentos SET ?`;
 
